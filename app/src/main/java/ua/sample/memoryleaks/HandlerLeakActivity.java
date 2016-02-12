14edf4ae5eee973405fa2530d1b0cf2856fff369
--replace-all <d.zhuchinskiy@smartweb.com.ua>
2016-02-12 21:04:25 +0200

    Unset LeakCanary

--- app/src/main/java/ua/sample/memoryleaks/HandlerLeakActivity.java
+++ app/src/main/java/ua/sample/memoryleaks/HandlerLeakActivity.java
@@ -5,60 +5,26 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 
-import java.lang.ref.WeakReference;
+import timber.log.Timber;
 
 public class HandlerLeakActivity extends AppCompatActivity {
+
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Timber.i("Trigger");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handler);
 
-        mLeakyHandler.postDelayed(sRunnable, 1000 * 60 * 10);
-
-        // Post a message and delay its execution for 10 minutes.
-//        mLeakyHandler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() { /* ... */ }
-//        }, 1000 * 60 * 10);
-
-        // Go back to the previous Activity.
-//        finish();
-
-
-    }
-
-    private static class SomeEventHandler extends Handler {
-        private final WeakReference<HandlerLeakActivity> mActivity;
-
-        public SomeEventHandler(HandlerLeakActivity activity) {
-            this.mActivity = new WeakReference<>(activity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-
-            HandlerLeakActivity activity = mActivity.get();
-            if (activity != null) {
-                // ...
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
             }
-        }
+        }, 1000 * 60 * 10);
     }
-
-    private final SomeEventHandler mLeakyHandler = new SomeEventHandler(this);
-
-//    private final Handler mLeakyHandler = new Handler() {
-//        HandlerLeakActivity activityWeakReference;
-//
-//        @Override
-//        public void handleMessage(Message msg) {
-//        }
-//    };
-
-    private static final Runnable sRunnable = new Runnable() {
-        @Override
-        public void run() { /* ... */ }
-    };
-
-
 }
--- app/src/main/java/ua/sample/memoryleaks/MemoryApplication.java
+++ app/src/main/java/ua/sample/memoryleaks/MemoryApplication.java
@@ -1,21 +1,22 @@
 package ua.sample.memoryleaks;
 
 import android.app.Application;
+import android.content.Context;
 
-import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
 public class MemoryApplication extends Application {
-    //    public static RefWatcher getRefWatcher(Context context) {
-//        MemoryApplication application = (MemoryApplication) context.getApplicationContext();
-//        return application.refWatcher;
-//    }
-//
+
+    public static RefWatcher getRefWatcher(Context context) {
+        MemoryApplication application = (MemoryApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
     private RefWatcher refWatcher;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        refWatcher = LeakCanary.install(this);
+//        refWatcher = LeakCanary.install(this);
     }
 }
